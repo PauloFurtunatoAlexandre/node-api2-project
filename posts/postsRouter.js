a0e0f00710998582d1b96efac8a67b69357c971c
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-05 13:46:54 -0500

    added put method and tests to make sure the post is correctly updated

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -85,4 +85,38 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  const postChanges = req.body;
+
+  if (postChanges.title && postChanges.contents) {
+    Posts.update(req.params.id, postChanges)
+      .then((post) => {
+        if (post) {
+          Posts.findById(req.params.id)
+            .then((post) => {
+              res.status(200).json(post);
+            })
+            .catch((err) => {
+              res.status(500).json({
+                errorMessage: "The post with the specified ID does not exist.",
+              });
+            });
+        } else {
+          res
+            .status(404)
+            .json({ error: "The post information could not be modified." });
+        }
+      })
+      .catch((err) => {
+        res
+          .status(404)
+          .json({ error: "The post information could not be modified." });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  }
+});
+
 module.exports = router;
