f58ee142aca7c8301bd324841447c7a28edd6270
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-09 23:04:36 -0500

    configured env file and dotenv to the project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const app = require("./app");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`App is running on port: ${port}`);
